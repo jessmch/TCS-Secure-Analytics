a1fc7be6f68c8ad8da238a8c4e5aed288f02f138
BirdmasterLance <8052295+BirdmasterLance@users.noreply.github.com>
2024-05-14 19:47:55 -0700

    Memdump changes for VMs

--- mem_dump.sh
+++ mem_dump.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-pid=$(pgrep -f "python main.py")
+pid=$(pgrep -f "python3 main.py")
 
 gcore -o memdump.core $pid >/dev/null 2>&1
-echo $(strings memdump.core.$pid)
+echo $(strings memdump.core.$pid > memdump_output.txt)
 
 #pid=$(pgrep -f "python crypt.py")
 #echo $(strings memdump.core.$pid | grep "sensitive information")
